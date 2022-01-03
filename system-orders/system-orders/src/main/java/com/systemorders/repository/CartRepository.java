20d3bc23d3f604bba79f869f56991d50f7770d0a
BaikaniKrishna <93122574+BaikaniKrishna@users.noreply.github.com>
2022-01-03 11:04:43 +0530

    Add files via upload

---
+++ system-orders/system-orders/src/main/java/com/systemorders/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package com.systemorders.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.systemorders.model.Cart;
+
+public interface CartRepository extends MongoRepository<Cart , String>{
+	List<Cart> findByDealerId(String dealerId);
+}
